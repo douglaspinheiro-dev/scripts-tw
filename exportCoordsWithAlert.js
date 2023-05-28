7e121eac0339bc77883e0d4a11ded28cd9ce9e02
Douglas Pinheiro <email@douglaspinheiro.dev>
2023-05-28 20:18:45 -0300

    update table

--- exportCoordsWithAlert.js
+++ exportCoordsWithAlert.js
@@ -1,4 +1,4 @@
-let tds = $('#villages_list > tbody > tr > td:nth-child(3)');let coords = [];for (let index = 0; index < tds.length; index++) {
+let tds = $('#villages_list > tbody > tr > td:nth-child(2)');let coords = [];for (let index = 0; index < tds.length; index++) {
     const element = tds[index];
     coords.push(element.textContent)
 };coords = coords.join(' ');alert(coords);
